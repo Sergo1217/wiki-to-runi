4fc2dabfcbd1e675557c30cf248962b5a8ede984
Сергей <kostikov.98@gmail.com>
2024-11-12 11:46:29 +0300

    Update wiki-to-runi.user.js

--- wiki-to-runi.user.js
+++ wiki-to-runi.user.js
@@ -3,7 +3,7 @@
 // @name:ru          Переадресация Вики на Руниверсалис
 // @description      Redirects Wikipedia to the Runi domain
 // @description:ru   Автоматическая переадресация Википедии на Руниверсалис
-// @version          1.0.1
+// @version          1.1.0
 // @author           Sergo1217
 // @match            *://*.wikipedia.org/*
 // @icon             https://руни.рф//resources/assets/logo-icon-new.svg
@@ -16,19 +16,28 @@
 
 (function() {
     'use strict';
-    const newURL = window.location.href.replace("ru.wikipedia.org/wiki", "руни.рф/index.php");
-    GM_xmlhttpRequest({
-        method: "HEAD",
-        url: newURL,
-        onload: function(response) {
-            if (response.status === 200) {
-                // If the final URL exists, redirect to it
-                window.location.replace(newURL);
+
+    // Проверка, что мы находимся на русском разделе Википедии
+    if (location.hostname === 'ru.wikipedia.org') {
+        // Заменяем часть URL для переадресации
+        const newURL = window.location.href.replace("ru.wikipedia.org/wiki", "руни.рф/index.php");
+
+        // Проверка доступности целевой страницы на Руниверсалис
+        GM_xmlhttpRequest({
+            method: "HEAD",
+            url: newURL,
+            onload: function(response) {
+                if (response.status === 200) {
+                    // Если страница существует, выполняем переадресацию
+                    window.location.replace(newURL);
+                } else {
+                    // Если страницы нет, выводим сообщение в консоль
+                    console.log("Страница на Руниверсалис не найдена:", newURL);
+                }
+            },
+            onerror: function(error) {
+                console.error("Ошибка запроса к Руниверсалис:", error);
             }
-            else {
-                // If the final URL does not exist, do nothing
-                console.log("The final URL does not exist.");
-            }
-        }
-    });
+        });
+    }
 })();
